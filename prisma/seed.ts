f39f68b5c0a688af58b0aff39e8e34801f0bcb88
hayasehiise <kuronegahiise@gmail.com>
2025-08-12 16:16:25 +0800

    sudah bisa seed 100 produk

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Produk_Status } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 async function main() {
   const hashedPassword = await bcrypt.hash('adminapp', 10);
-  const admin = await prisma.user.upsert({
+  await prisma.user.upsert({
     where: { username: 'adminapp' },
     update: {},
     create: {
@@ -19,7 +19,7 @@ async function main() {
     },
   });
   const hashedPasswordBackup = await bcrypt.hash('adminbackup', 10);
-  const adminBackup = await prisma.user.upsert({
+  await prisma.user.upsert({
     where: { username: 'adminbackup' },
     update: {},
     create: {
@@ -29,8 +29,19 @@ async function main() {
       role: 'ADMIN',
     },
   });
+  const products = Array.from({ length: 100 }).map((_, i) => ({
+    nama: `Produk ${i + 1}`,
+    harga: Math.floor(Math.random() * 100000) + 1000, // harga acak 1.000 - 100.000
+    stock: Math.floor(Math.random() * 100), // stok acak 0 - 99
+    status: Produk_Status.TERSEDIA,
+  }));
 
-  console.log({ admin, adminBackup });
+  await prisma.produk.createMany({
+    data: products,
+  });
+
+  // console.log({ admin, adminBackup });
+  console.log('data sudah dimasukan');
 }
 
 main()
